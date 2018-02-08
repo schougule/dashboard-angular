0b4169e0ef9cc519024a31dc316a13d43ecb6b5e
schougule <san007chougule@gmail.com>
2018-02-08 11:59:52 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {Observable} from 'rxjs/Rx';
 import { DbServiceService } from './service/db-service.service';
 import { differenceInCalendarDays, format } from 'date-fns'
 import { nextTick } from 'q';
+import { forkJoin } from 'rxjs/observable/forkJoin';
 
 @Component({
   selector: 'app-root',
@@ -46,44 +47,54 @@ export class AppComponent {
     
     
   ngAfterViewInit(){
-    this.service.getOfssaDetails().subscribe({
-      next : (value) =>{
-          this.ofssaData=value;
-            }
-        
-     }
-     )
+   var ofssDateObservable =this.service.getOfssaDetails();
+     
 
-    this.service.getUnhappyDetails().subscribe({
-      next : (value) =>{
-          this.unhappyData=value;
-            }
-        
-     }
-     )
+   var unhappyObservable =this.service.getUnhappyDetails();
+     
 
-    this.service.getVldatDetails().subscribe({
-      next : (value) =>{
-          this.vlDatData=value;
-            }
-        
-     }
-     )
+     var vldatObservable= this.service.getVldatDetails();
+     
+     var parentObservale= this.service.getParentDetails();
+    
 
-     this.service.getParentDetails().subscribe({
-      next : (value) =>{
-          this.parentData=value;
-          console.log("lenghth" +this.parentData.length +this.vlDatData.length);
-          for(var i=0;i<this.parentData.length;i++){
+
+     forkJoin(unhappyObservable,
+       
+       ofssDateObservable,
+       vldatObservable,
+       parentObservale)
+       .subscribe(([res1, res2, res3,res4]) => {
+
+        this.vlDatData =res3;
+        this.unhappyData=res1;
+        this.ofssaData=res2;
+        this.parentData=res4;
+
+        for(var i=0;i<this.parentData.length;i++){
+          
+          this.parentData[i].VLDAT=this.vlDatData[i].DATE;
+       this.parentData[i].Unhappy=this.unhappyData[i].DATE;
+          this.parentData[i].OFSAA=this.ofssaData[i].DATE;
+          }
+      
+     
+    });
+
+    //  this.service.getParentDetails().subscribe({
+    //   next : (value) =>{
+    //       this.parentData=value;
+    //       console.log("lenghth" +this.parentData.length +this.vlDatData.length);
+    //       for(var i=0;i<this.parentData.length;i++){
             
-            this.parentData[i].VLDAT=this.vlDatData[i].DATE;
-			   this.parentData[i].Unhappy=this.unhappyData[i].DATE;
-			      this.parentData[i].OFSAA=this.ofssaData[i].DATE;
-            }
-            }
+    //         this.parentData[i].VLDAT=this.vlDatData[i].DATE;
+		// 	   this.parentData[i].Unhappy=this.unhappyData[i].DATE;
+		// 	      this.parentData[i].OFSAA=this.ofssaData[i].DATE;
+    //         }
+    //         }
         
-     }
-     )
+    //  }
+    //  )
    
 
    
@@ -97,7 +108,8 @@ export class DashboardDTO {
 
   EOD_flow: string;
   Calendar: string;
-  Ofsset: string;
+  WD_Ofsset: string;
+  CD_Ofsset: string;
   File_set_cd: number;
   Draft: string;
   VLDAT: string;
